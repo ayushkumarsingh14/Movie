7aecfd02d469b143ffa2ee026e6315e86a1056fe
Ayush Kumar Singh <aaks.feb14@gmail.com>
2025-02-09 23:41:41 +0530

    bug fixed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 function Banner() {
     const bannerData = useSelector(state => state.movieData.bannerData)
@@ -72,9 +73,11 @@ function Banner() {
                                         <span>|</span>
                                         <p>Views: {Number(data.popularity).toFixed(0)}</p>
                                     </div>
-                                    <button className='bg-white px-4 rounded text-black py-2 font-bold mt-4 hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>
-                                        Play Now
-                                    </button>
+                                    <Link to={"/"+data?.media_type+"/"+data.id}>
+                                            <button  className=' bg-white px-4 py-2 text-black font-bold rounded mt-4  hover:bg-gradient-to-l from-red-700 to-orange-500 shadow-md transition-all hover:scale-105'>
+                                                Play Now
+                                            </button>
+                                     </Link>
                                 </div>
                         </div>
 
